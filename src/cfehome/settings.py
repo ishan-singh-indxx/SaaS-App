b394441d34f8c375a63467ac023b7a30a4fceaf2
Ishan Singh <isingh@indxx.com>
2025-04-30 11:47:03 +0530

    Added custom command to install CDN Automatically

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -42,6 +42,7 @@ if DEBUG:
 
 INSTALLED_APPS = [ 
     'visits',
+    'commando',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -135,6 +136,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR/ 'staticfiles'
+STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
+STATICFILES_VENDOR_DIR  = STATICFILES_BASE_DIR/'vendors'
+
+# source(s) for python manage.py collectstatic
+STATICFILES_DIRS = [STATICFILES_BASE_DIR]
+
+# output for python manage.py collectstatic
+# local cdn
+STATIC_ROOT = BASE_DIR /'local-cdn'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
