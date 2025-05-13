72f4c2419a40e8b4a10825e2161b71938d3b8858
Ishan Singh <isingh@indxx.com>
2025-05-13 10:11:15 +0530

    Sync User Subscription with the User Group

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,13 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import Group, Permission
 
-# Create your models here.
-class SubscriptionModel(models.Model):
-    name = models.CharField(max_length=120)
+from django.conf import settings
+User = settings.AUTH_USER_MODEL 
 
-    class Meta:
-        permissions = [
+from django.db.models.signals import post_save
+
+SUBSCRIPTION_PERMISSIONS = [
             ('advanced', 'Advanced Perm'), #subscriptions.advanced
             ('pro', 'Pro Perm'), #subscriptions.pro
             ('basic', 'Basic Perm'), #subscriptions.basic
             ('basic_ai', 'Basic AI Perm')
         ]
+
+# Create your models here.
+class SubscriptionModel(models.Model):
+    name = models.CharField(max_length=120)
+    active = models.BooleanField(default=True)
+    groups = models.ManyToManyField(Group)
+    permissions = models.ManyToManyField(Permission, 
+    limit_choices_to={'content_type__app_label': 'subscriptions',
+    "codename__in": [perm[0] for perm in SUBSCRIPTION_PERMISSIONS]
+        }
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        permissions = SUBSCRIPTION_PERMISSIONS
+
+class UserSubscriptionModel(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    subscription = models.ForeignKey(SubscriptionModel, 
+        on_delete=models.SET_NULL, null=True, blank=True)
+    active = models.BooleanField(default=True)
+
+def user_sub_post_save(sender, instance, *args, **kwargs):
+    user_sub_instance = instance
+    user = user_sub_instance.user
+    subscription_obj = user_sub_instance.subscription
+    groups = subscription_obj.groups.all()
+    user.groups.set(groups)
+
+post_save.connect(user_sub_post_save, sender=UserSubscriptionModel)
